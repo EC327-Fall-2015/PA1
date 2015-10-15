186e6e8650739437431aad68ac97e347d9878ba3
abeco <abecoh880@gmail.com>
2015-10-15 16:10:51 -0400

    PA1 Submission
    I don't understand how another version of code got jumbled in with mine,
    but mine is the part in green

--- Q1.cpp
+++ Q1.cpp
@@ -1,51 +1,25 @@
-
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
-#include <iomanip>
-
+#include <cmath> //sqrt function is in this library
 using namespace std;
-
-
-int main()
-{
-
-  // Prompt user to input 3 potential sides of a triangle
-  double sideOne, sideTwo, sideThree;
-  cout <<"Enter the dimensions of the triangle: " << endl;
-  cout << "Side-1: ";
-  cin >> sideOne;
-  cout << "Side-2: ";
-  cin >> sideTwo;
-  cout << "Side-3: ";
-  cin >> sideThree;
-
-                    
-
-  // Evaluate if the three sides can form a triangle, if they can't, display error message and exit program
-  if (sideOne <= 0 || sideTwo <= 0 || sideThree <= 0 ||
-      (sideOne + sideTwo) <= sideThree || (sideTwo + sideThree) <= sideOne || (sideOne + sideThree) <= sideTwo )
-  {
-    cout << "Dimensions " << fixed << setprecision(2) <<  sideOne << ", " << sideTwo << ", " << sideThree <<
-      " do not form a triangle." << endl;
-  }
-
-  // Calculate perimeter and area of triangle using Heron's formula
-  else
-  {
-    double perimeter = sideOne + sideTwo + sideThree;
-    double semiPerimeter = perimeter / 2;
-    double area = sqrt( semiPerimeter * (semiPerimeter - sideOne) * (semiPerimeter - sideTwo) * (semiPerimeter - sideThree) );
-    // cout << perimeter << ", " << semiPerimeter << ", " << area << endl;
-  
-    // display results and exit program
-    cout << "This triangle's area is " << fixed << setprecision(2) << area << " square-units, and its perimeter is " <<
-      perimeter << " units." << endl;
-  }
-
-
-
-
-
-  return 0;
+int main() {
+	double side1, side2, side3; //initialize vars for sides
+	cout << "Enter the dimensions of the triangle: " << endl;
+	cout << "Side-1: ";
+	cin >> side1;
+	cout << "Side-2: ";
+	cin >> side2;
+	cout << "Side-3: ";
+	cin >> side3;
+	//heron's formula if statement with check to see if sides form a triangle
+	if (side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2) {
+		double per = side1 + side2 + side3;
+		double hper = per / 2.0;
+		double area = sqrt(hper*(hper - side1)*(hper - side2)*(hper - side3));
+		cout << "The triangle's area is " << area << " square-units and its perimeter is " << per << " units." << endl;
+	}
+	//if sides are not compatible to form a triangle
+	else {
+		cout << "Dimensions " << side1 << ", " << side2 << ", " << side3 << " do not form a triangle.";
+	}
+	return 0;
 }
