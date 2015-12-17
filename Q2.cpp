0746b17c3f4e2d9c99ebe289a4abb5c67f996deb
nancyac1 <nancyac1@bu.edu>
2015-12-17 00:07:48 -0500

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,67 +1,80 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
-//Q2 solution
-// Made by Timothy Chong
+#include <iomanip>
 
 using namespace std;
 
-template <typename T>
-void get_num(T & num) {
-    num = -1;
-    cin >> num;
-    while (cin.fail()) {
-        cout << "Wrong input, try again: ";
-        cin.clear();
-        cin.ignore(100, '\n');
-        cin >> num;
+int main()
+{
+
+  cout << "Celsius to Farenheit(enter 0)"<< endl;
+  cout << "Celsius to Kelven(enter 1)" << endl;
+  cout << "Fahrenheit to Celsius(enter 2)" << endl;
+  cout << "Fahrenheit to Kelven(enter 3)" << endl;
+  cout << "Kelven to Fahrenheit(enter 4)" << endl;
+  cout << "Kelven to Celcius(enter 5)" << endl;
+  cout << "Conversion type:"<<endl;
+  int type;
+  cin>> type;
+  if (type == 0)
+    { 
+      cout<<" Enter amount in Celsius:"<<endl;
+      double temp1;
+      cin >> temp1;
+      double temp2;
+      temp2 = (temp1 * (9/5)) + 32;
+      cout << fixed;
+      cout << setprecision(3)<< temp1 << " Celcius is"<< temp2 << " Fahrenheit"<<endl;
     }
-}
-
-int main() {
-
-    string units[6][2];
-    units[0][0] = "Celsius";
-    units[1][0] = "Celsius";
-    units[2][0] = "Fahrenheit";
-    units[3][0] = "Fahrenheit";
-    units[4][0] = "Kelvin";
-    units[5][0] = "Kelvin";
-
-    units[0][1] = "Fahrenheit";
-    units[1][1] = "Kelvin";
-    units[2][1] = "Celsius";
-    units[3][1] = "Kelvin";
-    units[4][1] = "Celsius";
-    units[5][1] = "Fahrenheit";
-
-    //Printing options
-    for(int i = 0; i < 6; i++ ){
-        printf("%s to %s (enter %d)\n", units[i][0].c_str(), units[i][1].c_str(), i);
+  if (type == 1)
+    { 
+      cout<<" Enter amount in Celsius:"<<endl;
+      double temp1;
+      cin >> temp1;
+      double temp2;
+      temp2 = temp1 + 273.15;
+      cout << fixed;
+      cout << setprecision(3) << temp1 << " Celcius is"<< temp2 << " Kelvin"<<endl;
     }
-
-    //Getting input for type
-    cout << "Conversion type: ";
-    int type = -1;
-    get_num(type);
-    while (type < 0 || type > 5) {
-        cout << "Wrong input, try again: ";
-        get_num(type);
+  if (type == 2)
+    { 
+      cout<<" Enter amount in Farenheit:"<<endl;
+      double temp1;
+      cin >> temp1;
+      double temp2;
+      temp2 = (temp1 - 32) *(5/9);
+      cout << fixed;
+      cout << setprecision(3) << temp1 << " Fahreinheit is"<< temp2 << " Celcius"<<endl;
+    } 
+  if (type == 3)
+    { 
+      cout<<" Enter amount in Farenheit:"<<endl;
+      double temp1;
+      cin >> temp1;
+      double temp2;
+      temp2 = (temp1 + 459.67) * (5/9);
+      cout << fixed;
+      cout << setprecision(3) << temp1 << " Farenheit is"<< temp2 << " Kelvin"<<endl;
+    }  
+  if (type == 4)
+    { 
+      cout<<" Enter amount in Kelvin:"<<endl;
+      double temp1;
+      cin >> temp1;
+      double temp2;
+      temp2 = temp1 - 273.15;
+      cout << fixed;
+      cout << setprecision(3) << temp1 << " Kelvin is"<< temp2 << " Celcius"<<endl;
+    } 
+  if (type == 5)
+    { 
+      cout<<" Enter amount in Kelvin:"<<endl;
+      double temp1;
+      cin >> temp1;
+      double temp2;
+      temp2 = (temp1 * (9/5)) - 459.67;
+      cout << fixed;
+      cout << setprecision(3) << temp1 << " Kelvin is"<< temp2 << " Fahrenheit"<<endl;
+    } 
+  return 0; 
     }
-
-    cout << "Enter the amount in " << units[type][0] << ": ";
-    double input = -1;
-    get_num(input);
-    double output = -1;
-    switch(type){
-        case 0 : output = input * 9 / 5 + 32; break;
-        case 1 : output = input + 273.15; break;
-        case 2 : output = ( input - 32 ) * 5 / 9; break;
-        case 3 : output = (input - 32) * 5 / 9 + 273.15; break;
-        case 4 : output = input - 273.15; break;
-        case 5 : output = (input - 273.15) * 9 / 5 + 32; break;
-        default: cout << "Fatal error" << endl; exit(0);
-    }
-    printf("%.3f %s is %.3f %s.\n", input, units[type][0].c_str(), output, units[type][1].c_str());
-}
+      
